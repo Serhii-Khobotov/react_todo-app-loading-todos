7d72d6408c2fe5c86dbe11039d9e2cb4d4bd157c
Serhii-Khobotov <hobbitura@gmail.com>
2025-02-13 20:02:31 +0200

    my solution

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,57 +1,45 @@
-import { useState } from "react";
-import cn from 'classnames'
-import { Todo } from "../types/Todo"
+import cn from 'classnames';
+import { Todo } from '../types/Todo';
 
 type Props = {
   todos: Todo[];
-}
+};
 
 export const TodoList: React.FC<Props> = ({ todos }) => {
-  const [showErrorMessage, setShowErrorMessage] = useState(false);
-  const [isTodoActive, setIsTodoActive] = useState(false);
-
   return (
     <section className="todoapp__main" data-cy="TodoList">
       {/* This is a completed todo */}
-        {todos.map(todo => (
-          <div
+      {todos.map(todo => (
+        <div
           key={todo.id}
-            data-cy="Todo"
-            className={cn('todo', { 'completed': todo.completed === true})}
-          >
-            <label className="todo__status-label">
-              <input
-                data-cy="TodoStatus"
-                type="checkbox"
-                className="todo__status"
-                defaultChecked
-              />
-            </label>
+          data-cy="Todo"
+          className={cn('todo', { completed: todo.completed === true })}
+        >
+          <label className="todo__status-label">
+            <input
+              data-cy="TodoStatus"
+              type="checkbox"
+              className="todo__status"
+              checked={todo.completed}
+            />
+          </label>
 
-            <span
-              data-cy="TodoTitle"
-              className="todo__title"
+          <span data-cy="TodoTitle" className="todo__title">
+            {todo.title}
+          </span>
 
-            >
-              {todo.title}
-            </span>
+          {/* Remove button appears only on hover */}
+          <button type="button" className="todo__remove" data-cy="TodoDelete">
+            ×
+          </button>
 
-            {/* Remove button appears only on hover */}
-            <button
-              type="button"
-              className="todo__remove"
-              data-cy="TodoDelete"
-            >
-              ×
-            </button>
-
-            {/* overlay will cover the todo while it is being deleted or updated */}
-            <div data-cy="TodoLoader" className="modal overlay">
-              <div className="modal-background has-background-white-ter" />
-              <div className="loader" />
-            </div>
+          {/* overlay will cover the todo while it is being deleted or updated */}
+          <div data-cy="TodoLoader" className="modal overlay">
+            <div className="modal-background has-background-white-ter" />
+            <div className="loader" />
           </div>
+        </div>
       ))}
     </section>
-  )
-}
+  );
+};
